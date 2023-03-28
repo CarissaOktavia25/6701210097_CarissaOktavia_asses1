5fa75cac544aad68e7153dfe397801c8d904c8b9
CarissaOktavia25 <karissaoktavia756@gmail.com>
2023-03-28 22:30:21 +0700

    v1.2.1

--- proses.php
+++ proses.php
@@ -1,22 +1,23 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Ambil data yang dikirim dari form.html
-    $nilai1 = $_POST["nilai1"];
-    $nilai2 = $_POST["nilai2"];
-    $operator = $_POST["operator"];
-    
-    // Lakukan operasi aritmatika sesuai dengan operator yang dipilih
-    if ($operator == "+") {
-        $hasil = $nilai1 + $nilai2;
-    } elseif ($operator == "-") {
-        $hasil = $nilai1 - $nilai2;
-    } elseif ($operator == "*") {
-        $hasil = $nilai1 * $nilai2;
-    } elseif ($operator == "/") {
-        $hasil = $nilai1 / $nilai2;
-    }
-    
-    // Tampilkan hasil operasi aritmatika
-    echo "Hasil dari $nilai1 $operator $nilai2 adalah $hasil.";
-}
-?>
+  $angka1 = $_POST['angka1'];
+  $angka2 = $_POST['angka2'];
+  $operator = $_POST['operator'];
+
+  switch($operator) {
+    case '+':
+      $hasil = $angka1 + $angka2;
+      break;
+    case '-':
+      $hasil = $angka1 - $angka2;
+      break;
+    case '*':
+      $hasil = $angka1 * $angka2;
+      break;
+    case '/':
+      $hasil = $angka1 / $angka2;
+      break;
+    default:
+      $hasil = "Operasi tidak valid";
+  }
+
+  echo "Hasil operasi $angka1 $operator $angka2 adalah $hasil";
